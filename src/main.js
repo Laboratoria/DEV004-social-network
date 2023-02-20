b6babf07c51c9d1be6b34fadd55b5b9c99bf0464
AlexVYard <alexvegayard@gmail.com>
2023-02-20 09:42:25 -0500

    fixed background and small text fix

--- src/main.js
+++ src/main.js
@@ -10,13 +10,14 @@ function iniciar(){
   <main class="PantallaInicio">
     <section class="cajaInicio">
       <img src="./img/img_libro_rojo.png" alt="Imagen de libro">
-      <input type="text" placeholder="Nombre o Nick" id="inputNick">
-      <input type="text" placeholder="E-mail" id="inputEmail">
-      <input type="password" placeholder="Password" id="inputEmail">
+      <input type="text" placeholder=" Nombre o Nick" id="inputNick">
+      <input type="text" placeholder=" E-mail" id="inputEmail">
+      <input type="password" placeholder=" Password" id="inputEmail">
       <button id="botonInicio">Log in</button>
-      <span>------------- OR -------------</span>
+      <hr style="width:100%;text-align:center">
       <button id="botonInicioGoogle"><img src="./img/btn_google_signin.png" alt="boton de google" class="imgButton"></button>
-      <a id="botonRegistrar" href="#">Register</a>
+      <p class="textoCrearCuenta">Dont have an account?
+      <a id="botonRegistrar" href="#"> Register</a></p>
     </section>
   </main> 
   `
