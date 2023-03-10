118a1285aacde6ea987100429de5be5742932baf
katalimbu <katalina.esteban@gmail.com>
2023-03-10 15:26:42 -0300

    union de las rutas sin resultado aun

--- src/main.js
+++ src/main.js
@@ -1,10 +1,36 @@
 
+import { home } from './componets/home.js';
 import { login } from './componets/login.js';
-
 import { register } from './componets/register.js';
 
-const component = register();
-const rootDiv = document.getElementById('root');
-rootDiv.append(component);
+import {
+    // navigate,
+    setRoutes,
+    handlePopState,
+    renderComponent,
+  } from './index.js'
 
+  const routes = [
+    {
+      pathname: '/',
+      component: home,
+    },
+    {
+      pathname: '/register',
+      component: register,
+    },
+    {
+      pathname: '/login',
+      component: login,
+    },
+    // {
+    //   pathname: '*', // ruta por defecto
+    //   component: NotFound,
+    // },
+  ]
+  
 
+// const component = login();
+// const rootDiv = document.getElementById('root');
+// rootDiv.append(component)
+// rootDiv.innerHTML = routes[window.location.pathname];
