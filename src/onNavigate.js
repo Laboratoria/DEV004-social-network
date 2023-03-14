31af954a334f0f07e541cd7e7d0c63813878e80e
TaniaGJimenez <122074803+TaniaGJimenez@users.noreply.github.com>
2023-03-14 14:29:50 -0600

    Resolviendo Conflictos

--- src/onNavigate.js
+++ src/onNavigate.js
@@ -1,14 +1,39 @@
-// Este objeto se utiliza para almacenar
-// las rutas y los manejadores de las páginas de la aplicación.
-const ROUTES = {};
+import { home } from './components/home.js';
+import { register } from './components/register.js';
 
-export const onNavigate = (pathname) => {
-  const path = typeof ROUTES[pathname] !== 'function' ? pathname : '/';
-  window.history.pushState({}, path, window.location.origin + pathname);
-  const rootSection = document.getElementById('root');
-  rootSection.innerHTML = '';
-  rootSection.append(ROUTES[pathname]());
+const root = document.getElementById('root');
+const routes = {
+  '/': home,
+  '/register': register,
 };
+const onNavigate = (pathname, paramRoutes = routes) => {
+  window.history.pushState({}, pathname, window.location.origin + pathname);
+  root.removeChild(root.firstChild);
+  root.appendChild(paramRoutes[pathname]);
+};
+const component = routes[window.location.pathname];
+window.onpopstate = () => {
+  root.removeChild(root.firstChild);
+  root.appendChild(component(onNavigate));
+};
+
+root.appendChild(component(onNavigate));
+// // aqui exportaras las funciones que necesites
+// const routes = {
+//   '/': home,
+//   '/register': register,
+// };
+// const rootDiv = document.getElementById('root');
+
+// export const onNavigate = (pathname) => {
+//   window.history.pushState(
+//     {},
+//     pathname,
+//     window.location.origin + pathname,
+//   );
+//   rootDiv.appendChild(routes[pathname]);
+// };
+
 
 export const addRoutes = (routes) => {
   Object.keys(routes).reduce((currentRoutes, pathname) => {
---
+++ src/routers.js
@@ -0,0 +1,12 @@
+// import { home } from './components/home';
+// import { register } from './components/register.js';
+
+// export const routes = {
+//   '/': home,
+//   '/register': register,
+// };
+
+// const rootDiv = document.getElementById('root');
+
+// const component = routes[window.location.pathname];
+// rootDiv.appendChild(component());
