bb860294c7d6371035d02ddb5893f73d75058b7b
Esperaanza <xcaanda100@gmail.com>
2023-03-14 11:32:07 -0600

    SPA

--- src/routers.js
+++ src/routers.js
@@ -1,12 +1,15 @@
 import { home } from './components/home';
-import { register } from './components/register.js';
+import { register } from './components/register';
+import { login } from './components/login';
 
 export const routes = {
   '/': home,
   '/register': register,
+  '/login': login,
 };
 
 const rootDiv = document.getElementById('root');
 
 const component = routes[window.location.pathname];
+
 rootDiv.appendChild(component());
