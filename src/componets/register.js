f348953c813ceeba980c0424c5a9b5cbc29fd6aa
LuzAlvaca <luz.alvaca@gmail.com>
2023-03-21 23:43:22 -0600

    estilos vista home corrección en vista register

--- src/componets/register.js
+++ src/componets/register.js
@@ -4,7 +4,7 @@ import { navigateTo } from '../router.js';
 export const register = () => {
   const div = document.createElement('div');
   div.innerHTML = `<form id="registerForm">
-    <div class="container">
+    <div class="containerRegister">
       <h1>Registro de Usuario</h1>
 
       <label for="name"><b>Nombre</b></label>
@@ -35,17 +35,17 @@ export const register = () => {
       </div>
     </div>
   </form>`;
-
-  div.querySelector('#registerForm').addEventListener('submit', (e) => {
+  // aca metí el form en una constante para que sea mas claro
+  const form = div.querySelector('#registerForm');
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('hola', e.target);
-    const email = e.target.elements.email.value;
-    const password = e.target.elements.psw.value;
 
+    console.log('hola', e.target);
+    const email = e.target.email.value;
+    const password = e.target.psw.value;
     registerWithEmail(email, password)
       .then((useCredential) => {
         navigateTo('/home');
-        console.log(useCredential);
       })
       .catch((error) => {
         console.log(error.message);
@@ -62,83 +62,6 @@ export const register = () => {
         }
       });
   });
-
-  return div;
+  // pintar el formulario así en vez de "return div" es mas claro y funciona,
+  return form;
 };
-
-//   const HomeDiv = document.createElement('div');
-//   HomeDiv.innerHTML = ('Registro de usuario');
-//   const divForm = document.createElement('form');
-//   HomeDiv.appendChild(divForm);
-
-//   const inputName = document.createElement('input');
-//   inputName.setAttribute('type', 'text');
-//   inputName.setAttribute('placeholder', 'nombre');
-
-//   const inputPassword = document.createElement('input');
-//   inputPassword.setAttribute('type', 'text');
-//   inputPassword.setAttribute('placeholder', 'contraseña');
-
-//   const inputMail = document.createElement('input');
-//   inputMail.setAttribute('type', 'text');
-//   inputMail.setAttribute('placeholder', 'Correo Electrónico');
-
-//   const inputNationality = document.createElement('input');
-//   inputNationality.setAttribute('type', 'text');
-//   inputNationality.setAttribute('placeholder', 'Nacionalidad');
-
-//   const inputDateOfBirth = document.createElement('input');
-//   inputDateOfBirth.setAttribute('type', 'text');
-//   inputDateOfBirth.setAttribute('placeholder', 'Fecha de nacimiento');
-
-//   const inputOcupation = document.createElement('input');
-//   inputOcupation.setAttribute('type', 'text');
-//   inputOcupation.setAttribute('placeholder', 'Ocupación');
-
-// const selectRolReda = document.createElement("select");
-// const optionRol = document.createElement("option");
-// const optionEspecialist = document.createElement("option");
-// const optionCarer = document.createElement("option");
-
-// optionRol.value = " ";
-// optionRol.text = "--Elige tu Rol en Reda--";
-
-// optionEspecialist.value = "Especialist";
-// optionEspecialist.text = "Especialista";
-
-// optionCarer.value = "Carer";
-// optionCarer.text = "Cuidador";
-
-// selectRolReda.add(optionRol, null);
-// selectRolReda.add(optionEspecialist, null);
-// selectRolReda.add(optionCarer, null);
-
-// optionEspecialist.value = "Especialist";
-// optionEspecialist.text = "Especialista";
-
-// optionEspecialist.value = "Especialist";
-// optionEspecialist.text = "Especialista";
-
-// optionEspecialist.value = "Especialist";
-// optionEspecialist.text = "Especialista";
-
-// const selectEspecialist = document.createElement("select");
-// const optionPsichologyst = document.createElement("option");
-// const optionTeacher = document.createElement("option");
-// const optionDoctor = document.createElement("option");
-
-//   const buttonSend = document.createElement('button');
-//   buttonSend.textContent = 'enviar';
-
-//   divForm.appendChild(inputName);
-//   divForm.appendChild(inputPassword);
-//   divForm.appendChild(inputMail);
-//   divForm.appendChild(inputNationality);
-//   divForm.appendChild(inputDateOfBirth);
-//   divForm.appendChild(inputOcupation);
-//   divForm.appendChild(selectRolReda);
-//   divForm.appendChild(buttonSend);
-
-//   return HomeDiv;
-
-// };
