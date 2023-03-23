67351d9519e8ac6cf443ce5b832b7e568c1fb751
katalimbu <katalina.esteban@gmail.com>
2023-03-23 15:01:49 -0300

    ordenado de los divs de login para usarlos en css

--- src/componets/login.js
+++ src/componets/login.js
@@ -5,22 +5,16 @@ export const Login = () => {
   // Create a div element to hold the login component
   const div = document.createElement('div');
   div.innerHTML = `<form id="loginForm">
-  <div class='container'>
-
-    <div class='loginTitle'>
-      <h1>Ingresa a Reda</h1>
+    <div class='logo'>
+    <img src= 'https://i.ibb.co/bWGQN64/REDA-1.png' class = 'logoReg' alt = 'Logo Reda'>
     </div>
-
+    <h1>Ingresa a Reda</h1>
     <div class="imputLogin">
       <label  for="username"><b>Correo electrónico</b></label>
       <input type="text" placeholder="Correo electrónico" name="username" id="username" required>
+      <label for="psw"><b>Contraseña</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
     </div>
-    
-    <div>
-    <label class="imputLogin" for="psw"><b>Contraseña</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
-    </div>
-
    <div class="sendBtn">
     <button class="enviar btn">Enviar</button>
     </div>
@@ -29,8 +23,6 @@ export const Login = () => {
       <a href="#" class="google btn"><i  class="fa fa-google fa-fw">
             </i> <img src= 'https://i.ibb.co/1nDpBf4/btn-google-signin-light-pressed-web.png'></a>
     </div>
-</div>
-</div>
 </div>
     <div class="modal">
       <div class="modal-content">
@@ -43,7 +35,7 @@ export const Login = () => {
   const openModal = (message) => {
     div.querySelector('.modal').style.display = 'block';
     div.querySelector('.modal-content > p:nth-child(2)').textContent = message;
-  };
+  };//cambiar el text contente por el inner para probar que pasa 
 
   // Add event listeners to the login component
 
@@ -73,7 +65,15 @@ export const Login = () => {
           navigateTo('/home');
         },
         (error) => {
-          openModal(error.message);
+          if (error.code === 'auth/invalid-email') {
+            alert('Invalid Email');//cambiar los alert por inner y no es equivalente al modal
+          } else if (error.code === 'auth/weak-password') {
+            alert('password is too weak');
+          } else if (error.code === 'auth/email-already-in-use') {
+            alert('Email already in use');
+          } else if (error.code) {
+            alert('Something went wrong');
+          }
         },
       );
   });
