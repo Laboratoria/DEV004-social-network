d7c5cd2030480f4d9350b077f8763f59496a3d9a
Esperaanza <xcaanda100@gmail.com>
2023-04-03 16:10:59 -0600

    avence de likes

--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -8,6 +8,7 @@ import {
   deleteDoc,
   doc,
   updateDoc,
+  arrayUnion,
 } from 'firebase/firestore';
 import { app } from './firebaseConfig';
 
@@ -15,16 +16,17 @@ const db = getFirestore(app);
 
 const getTimestamp = () => serverTimestamp();
 
-const savePublic = (publicacion, likes, name, time) => addDoc(collection(db, 'publication'), {
-  publicacion, likes, name, time,
+const savePublic = (publicacion, likes, usersLikeArray, name, time) => addDoc(collection(db, 'publication'), {
+  publicacion, likes, usersLikeArray, name, time,
 });
 const postData = () => query(collection(db, 'publication'), orderBy('time', 'desc'));
 export const deletePost = (id) => deleteDoc(doc(db, 'publication', id));
 export const updatePost = (id, newDocument) => updateDoc(doc(db, 'publication', id), newDocument);
-
+const like = (id, likes, userLike) => updateDoc(doc(db, 'Publication', id), { likes, usersLikeArray: arrayUnion(userLike) });
 export {
   db,
   savePublic,
   postData,
   getTimestamp,
+  like,
 };
