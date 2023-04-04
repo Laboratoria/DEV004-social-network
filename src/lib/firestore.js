bdb7259a79913175cb2cb192d24d6c4c2243d35d
Lau Florez <lauraflorez1627@hotmail.com>
2023-04-04 10:41:07 -0500

    limpieza de codigo y fix de likes

--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -16,13 +16,22 @@ const db = getFirestore(app);
 
 const getTimestamp = () => serverTimestamp();
 
-const savePublic = (publicacion, likes, usersLikeArray, name, time) => addDoc(collection(db, 'publication'), {
-  publicacion, likes, usersLikeArray, name, time,
+const savePublic = (publicacion, likes, usersLike, name, time) => addDoc(collection(db, 'publication'), {
+  publicacion, likes, usersLike, name, time,
 });
 const postData = () => query(collection(db, 'publication'), orderBy('time', 'desc'));
 export const deletePost = (id) => deleteDoc(doc(db, 'publication', id));
 export const updatePost = (id, newDocument) => updateDoc(doc(db, 'publication', id), newDocument);
-const like = (id, likes, userLike) => updateDoc(doc(db, 'Publication', id), { likes, usersLikeArray: arrayUnion(userLike) });
+
+const like = (docum, auth) => {
+  // Obtener la referencia al documento correspondiente en Firebase
+  const docRef = doc(db, 'publication', docum.id);
+
+  // Incrementar el contador de likes y actualizar en Firebase
+  // const newLikes = docum.data().cantidaddelikes + 1;
+  return updateDoc(docRef, { likes: arrayUnion(auth.currentUser.uid) })
+};
+    
 export {
   db,
   savePublic,
