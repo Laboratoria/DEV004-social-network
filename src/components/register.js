0fce9e7ffb2052da825ea31eed719983f971cb73
TaniaGJimenez <122074803+TaniaGJimenez@users.noreply.github.com>
2023-04-05 10:30:19 -0600

    Agrega mensaje de confirmación al eliminar publicación

--- src/components/register.js
+++ src/components/register.js
@@ -82,8 +82,8 @@ export const register = (onNavigate) => {
 
     try {
       const UserCredentials = await registerWithEmail(email, password);
-      console.log({UserCredentials});
-      console.log("currentUser", auth.currentUser);
+      console.log({ UserCredentials });
+      console.log('currentUser', auth.currentUser);
       updateProfile(auth.currentUser, {
         displayName: nameInput.value,
         // photoURL: 'https://example.com/john-doe/profile.jpg',
