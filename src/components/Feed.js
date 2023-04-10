9fd2e28c9b6941b8f22f641f30465c3447baaae9
Maria Garcia <licgarcia_psic@hotmail.com>
2023-04-10 12:39:18 -0600

    bienvenida Login

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,13 +4,13 @@
 export const Feed = () => {
   const HomeDiv = document.createElement('div');
   const h3 = document.createElement('h3');
-  h3.textContent = 'Bienvenida al login';
+  h3.textContent = 'Bienvenida MaMa Geniales';
   const buttonHome = document.createElement('button');
   const buttonLoginGoogle = document.createElement('button');
 
   buttonHome.textContent = 'Regresar al Home';
   buttonHome.addEventListener('click', () => onNavigate('/'));
-  buttonLoginGoogle.addEventListener('click', () => onNavigate('/loginGoogle'));
+  buttonLoginGoogle.addEventListener('click', () => onNavigate('/feed'));
 
   //HomeDiv.append(h3,buttonHome, buttonLoginGoogle); este lo comentamos al final y pusimos el return h3
   //HomeDiv.appendChild(buttonLogin);
