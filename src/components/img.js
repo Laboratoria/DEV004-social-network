779a7449fd57aeaed395cebd2ba78865b63d1ca5
Esperaanza <xcaanda100@gmail.com>
2023-04-12 19:16:59 -0600

    Fix de archivo de barril

--- src/components/img.js
+++ src/components/img.js
@@ -1,12 +1,12 @@
-import { slogan } from '../Img/amolosanimales.png';
-import { googleAccess } from '../Img/BtnGoogle.png';
-import { welcomePic } from '../Img/CircleLogo.png';
-import { homeHome } from '../Img/homeIcon.png';
-import { likedIcon } from '../Img/likePaw.png';
-import { likeIcon } from '../Img/likePawZero.png';
-import { logo } from '../Img/LogoPetropolisSF.png';
-import { outOut } from '../Img/LogOutIcon.png';
-import { profileProfile } from '../Img/profileIcon.png';
+import slogan from '../Img/amolosanimales.png';
+import googleAccess from '../Img/BtnGoogle.png';
+import welcomePic from '../Img/CircleLogo.png';
+import homeHome from '../Img/homeIcon.png';
+import likedIcon from '../Img/likePaw.png';
+import likeIcon from '../Img/likePawZero.png';
+import logo from '../Img/LogoPetropolisSF.png';
+import outOut from '../Img/LogOutIcon.png';
+import profileProfile from '../Img/profileIcon.png';
 
 export {
   slogan, googleAccess, welcomePic, homeHome, likedIcon, likeIcon, logo, outOut, profileProfile,
