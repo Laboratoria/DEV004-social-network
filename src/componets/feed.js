b91c971ce2027d08b7ac80e170a71d4479c7e580
katalimbu <katalina.esteban@gmail.com>
2023-04-18 23:46:31 -0400

    funcionan iconos menos gaurdar

--- src/componets/feed.js
+++ src/componets/feed.js
@@ -111,131 +111,136 @@ export const feed = () => {
     // console.log(feedTitle);
     // console.log(feedPost);
     // acá es importante el orden de los atributos, par saber como se imprimen.
-    createpost(feedUser, feedTitle, feedPost)
+    const dibujar = () => {
+      const myPromise = getpost();
+      // const formularioPost = document.getElementById('squareFPerro');
+      squareF.innerHTML = '';
+      myPromise.then((showPost) => {
+      // nos aseguramos que la data provenga de feed.js y no de
+      // firebase.
+        // console.log('feed', showPost);
+        showPost.filter((p) => p.likes !== undefined).forEach((postD) => {
+          // const postForm = document.createElement('form');
+          const form = document.createElement('form');
+          form.classList.add('formularioEditar');
+          form.setAttribute('data-id', postD.id);
+          form.setAttribute('id', 'form');
 
+          form.innerHTML = `<div class="contenedorCurrentUser"> <text disabled>
+          Autor: ${postD.usuario}
+          </text>
+          <div class="contenedorPostCompleto">
+          <input name="titulo" id="titulo-${postD.id}" class="tituloEdit" value="${postD.titulo}" disabled />
+          <textarea name="descripcion" class="descriptionEdit" id="${postD.id}"  disabled />
+  ${postD.descripcion}
+          </textarea>
+          </div>
+          <div class="contenedorIconosPost">
+          <ion-icon name="trash-outline" type="button" id="btnDeletePost" class="${auth.currentUser.email === postD.usuario ? 'show' : 'noShow'}" data-id="${postD.id}" value="Borrar"></ion-icon>
+          <ion-icon name="create-outline" type="button" id="btnEditPost" value="Editar" class="${auth.currentUser.email === postD.usuario ? 'show' : 'noShow'}" data-id="${postD.id}"></ion-icon> 
+          <ion-icon name="save-outline" type="submit" id="btnSaveEditPost" value="Guardar" class="${auth.currentUser.email === postD.usuario ? 'show' : 'noShow'}"></ion-icon>
+          <ion-icon class="corazonIcon" name="${(postD.likes || []).includes(auth.currentUser.email) ? 'heart' : 'heart-outline'}" id="like-${postD.id}" ></ion-icon>  ${postD.likes.length}.
+          </div>
+          <div class="nuevoPostContainer">
+          <a href="/feed">publicar nuevamente</a> 
+          </div>
+          
+          
+          </div>`;
+          // Oh con coni:?? significa que si el primer arreglo no existe
+          // retorna el array vacio.(nullish)
+          // <ion-icon name="heart-outline"></ion-icon>
+          // ojo con los dos signos de interrogaciòn juntos, no son un operador ternario
+          // <ion-icon class="like"  name="${(postD.likes ?? []).includes(auth.currentUser.email)?
+          // 'heart' :'heart-outline' }" data-id="${postD.id}"></ion-icon>
+          console.log(form);
+          form.addEventListener('submit', (event) => {
+            console.log("submit!");
+            event.preventDefault();
+            // guardar en firebase
+            const postId = form.dataset.id;
+            const newTitle = event.target.elements.titulo.value;
+            const newDescription = event.target.elements.descripcion.value;
+            const newtitleEd = document.querySelector('.tituloEdit');
+            const newdescripEd = document.querySelector('.descriptionEdit');
+            const newPost = {
+              titulo: newTitle,
+              descripcion: newDescription,
+            };
+            console.log(postId);
+            console.log(newPost);
+            updatePost(postId, newPost);
+            newtitleEd.setAttribute('disabled', '');
+            console.log(newtitleEd);
+            newdescripEd.setAttribute('disabled', '');
+          });
+          const like = form.querySelector(`#like-${postD.id}`);
+          // console.log(like);
+          like.addEventListener('click', (event) => {
+            event.preventDefault();
+            // console.log(e);
+            // console.log(auth.currentUser.email);
+            // este like representa un booleano
+            const postLike = (postD.likes || []).includes(auth.currentUser.email);
+            if (postLike === true) {
+              const newLikes = postD.likes.filter((email) => email !== auth.currentUser.email);
+              updatelike(postD.id, newLikes).then(() => {
+                dibujar();
+              });
+            } else {
+              addLike(postD.id, [...postD.likes, auth.currentUser.email]).then(() => {
+                console.log(postD.likes, 'esto es el array de likes');
+                dibujar();
+                console.log(postD.likes, 'esto es el array de likes');
+              });
+            }
+          });
+          squareF.appendChild(form);
+        });
+
+        const btnsDeletePost = document.querySelectorAll('#btnDeletePost');
+        console.log(btnsDeletePost);
+        btnsDeletePost.forEach((btn) => {
+          btn.addEventListener('click', (event) => {
+            event.preventDefault();
+            const btnId = btn.getAttribute('data-id');
+            // console.log(btnId);
+            const shouldDeletePost = window.confirm('¿Estás seguro de que deseas eliminar este post?');
+            if (shouldDeletePost) {
+              const formToRemove = document.getElementById('form');
+              formToRemove.remove();
+              // console.log(formToRemove);
+              deletePost(btnId);
+            }
+          });
+        });
+
+        const btnsEditar = document.querySelectorAll('#btnEditPost');
+        btnsEditar.forEach((btn) => {
+          btn.addEventListener('click', (event) => {
+            event.preventDefault();
+            console.log(e);
+            const postId = btn.getAttribute('data-id');
+            const postTitulo = `titulo-${postId}`;
+            const textAreaPublication = document.getElementById(postId);
+            const inputPublication = document.getElementById(postTitulo);
+            console.log(inputPublication);
+            textAreaPublication.removeAttribute('disabled');
+            inputPublication.removeAttribute('disabled');
+          });
+        });
+      });
+    };
+
+    createpost(feedUser, feedTitle, feedPost)
     // el dibjuar post debe estar dentro de una promesa para que dibuje
       .then(() => {
-        dibujar( );
+        dibujar();
         // document.querySelector('class', 'postContainer').reset();
         postContainer.reset();
       });
     // revisar este error para de dibujar;
   });
-  const dibujar = () => {
-    const myPromise = getpost();
-    // const formularioPost = document.getElementById('squareFPerro');
-    squareF.innerHTML = '';
-    myPromise.then((showPost) => {
-    // nos aseguramos que la data provenga de feed.js y no de
-    // firebase.
-      // console.log('feed', showPost);
-      showPost.forEach((postD) => {
-        // const postForm = document.createElement('form');
-        const form = document.createElement('form');
-        form.classList.add('formularioEditar');
-        form.setAttribute('data-id', postD.id);
-        form.setAttribute('id', 'form');
 
-        form.innerHTML = `<div class="contenedorCurrentUser"> <text disabled>
-        Autor: ${postD.usuario}
-        </text>
-        <div class="contenedorPostCompleto">
-        <input name="titulo" id="titulo-${postD.id}" class="tituloEdit" value="${postD.titulo}" disabled />
-        <textarea name="descripcion" class="descriptionEdit" id="${postD.id}"  disabled />
-${postD.descripcion}
-        </textarea>
-        </div>
-        <div class="contenedorIconosPost">
-        <ion-icon name="trash-outline" type="button" id="btnDeletePost" class="${auth.currentUser.email === postD.usuario ? 'show' : 'noShow'}" data-id="${postD.id}" value="Borrar"></ion-icon>
-        <ion-icon name="create-outline" type="button" id="btnEditPost" value="Editar" class="${auth.currentUser.email === postD.usuario ? 'show' : 'noShow'}" data-id="${postD.id}"></ion-icon> 
-        <ion-icon name="save-outline" type="submit" id="btnSaveEditPost" value="Guardar" class="${auth.currentUser.email === postD.usuario ? 'show' : 'noShow'}"></ion-icon>
-        <ion-icon class="corazonIcon" name="${(postD.likes ?? []).includes(auth.currentUser.email)? 'heart' :'heart-outline' }" id="like-${postD.id}" ></ion-icon>  ${postD.likes.length}.
-        <div class="nuevoPostContainer">
-        <a href="/feed">publicar nuevamente</a> 
-        </div>
-        
-        
-        </div>`;
-        // Oh con coni:?? significa que si el primer arreglo no existe, retorna el array vacio.(nullish)
-        //<ion-icon name="heart-outline"></ion-icon>
-        // ojo con los dos signos de interrogaciòn juntos, no son un operador ternario
-        // <ion-icon class="like"  name="${(postD.likes ?? []).includes(auth.currentUser.email)?
-        // 'heart' :'heart-outline' }" data-id="${postD.id}"></ion-icon>
-        form.addEventListener('submit', (e) => {
-          e.preventDefault();
-          // guardar en firebase
-          const postId = form.dataset.id;
-          const newTitle = e.target.elements.titulo.value;
-          const newDescription = e.target.elements.descripcion.value;
-          const newtitleEd = document.querySelector('.tituloEdit');
-          const newdescripEd = document.querySelector('.descriptionEdit');
-          const newPost = {
-            titulo: newTitle,
-            descripcion: newDescription,
-          };
-          // console.log(postId);
-          // console.log(newPost);
-          updatePost(postId, newPost);
-          newtitleEd.setAttribute('disabled', '');
-          console.log(newtitleEd);
-          newdescripEd.setAttribute('disabled', '');
-        });
-        const like = form.querySelector(`#like-${postD.id}`);
-        // console.log(like);
-        like.addEventListener('click', (e) => {
-          e.preventDefault();
-          // console.log(e);
-          // console.log(auth.currentUser.email);
-        // este like representa un booleano
-          const like = (postD.likes ?? []).includes(auth.currentUser.email);
-          if (like === true) {
-            const newLikes = postD.likes.filter((email) => email !== auth.currentUser.email);
-            updatelike(postD.id, newLikes).then(() => {
-              dibujar();
-            });         
-          }
-          else {
-            addLike(postD.id, [...postD.likes, auth.currentUser.email]).then(() => {
-              console.log(postD.likes, 'esto es el array de likes');
-              dibujar();
-              console.log(postD.likes, 'esto es el array de likes');
-            });
-          }
-        });
-        squareF.appendChild(form);
-      });
-      const btnsDeletePost = document.querySelectorAll('#btnDeletePost');
-      console.log(btnsDeletePost);
-      btnsDeletePost.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-          e.preventDefault();
-          const btnId = btn.getAttribute('data-id');
-          // console.log(btnId);
-          const shouldDeletePost = window.confirm('¿Estás seguro de que deseas eliminar este post?');
-          if (shouldDeletePost) {
-            const formToRemove = document.getElementById('form');
-            formToRemove.remove();
-            // console.log(formToRemove);
-            deletePost(btnId);
-          }
-        });
-      });
-
-      const btnsEditar = document.querySelectorAll('#btnEditPost');
-      btnsEditar.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-          e.preventDefault();
-          console.log(e);
-          const postId = btn.getAttribute('data-id');
-          const postTitulo = `titulo-${postId}`;
-          const textAreaPublication = document.getElementById(postId);
-          const inputPublication = document.getElementById(postTitulo);
-          console.log(inputPublication);
-          textAreaPublication.removeAttribute('disabled');
-          inputPublication.removeAttribute('disabled');
-        });
-      });
-    });
-  };
   return squareF;
 };
