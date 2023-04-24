60a2ba41dd8aca0cfc781e0638ae7b9e3f332e51
Carolina Paredes Arias <carolina.paredes.arias@gmail.com>
2023-04-24 16:11:58 -0400

    Se arreglan cambios que por error eliminé

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,53 +1,58 @@
-
 export const register = () => {
   // crea contenedor principal
-  const article= document.createElement("article");
-  article.setAttribute("id","principal2");
+  const article = document.createElement('article');
+  article.setAttribute('id', 'principal2');
 
-  const body = document.createElement("body");
-  body.setAttribute("id", "container2");
+  const body = document.createElement('body');
+  body.setAttribute('id', 'container2');
 
-  const img = document.createElement("img");//icono para subir foto
-  img.src = "imagenes/imagenfoto.png";
-  img.classList.add("foto");
+  const img = document.createElement('img'); // icono para subir foto
+  img.src = 'imagenes/imagenfoto.png';
+  img.classList.add('foto');
 
-  const inputFile = document.createElement("input"); //boton examinar
-  inputFile.setAttribute("type","file");
-  inputFile.onchange = (e) =>{ //subir foto
-    if (inputFile.files[0])
-    img.src =URL.createObjectURL(inputFile.files[0]);
-  }
-  
+  const inputFile = document.createElement('input'); // boton examinar
+  inputFile.setAttribute('type', 'file');
+  inputFile.onchange = (e) => {
+    // subir foto
+    if (inputFile.files[0]) img.src = URL.createObjectURL(inputFile.files[0]);
+  };
 
+  const Nombre = document.createElement('h1'); // modifica propiedades de los elemento
+  Nombre.textContent = 'Ingresa el nombre de tu mascota'; // retorna el elemento
 
-  const Nombre = document.createElement("h1"); // modifica propiedades de los elemento
-  Nombre.textContent = "Ingresa el nombre de tu mascota"; // retorna el elemento
-
-  const Raza =document.createElement('h1');
-  Raza.textContent ='Raza de tu mascota';
+  const Raza = document.createElement('h1');
+  Raza.textContent = 'Raza de tu mascota';
 
   const Edad = document.createElement('h1');
   Edad.textContent = 'Edad de tu mascota';
 
-  const inputNombre = document.createElement("INPUT");
-  inputNombre.classList.add("box");
+  const inputNombre = document.createElement('INPUT');
+  inputNombre.classList.add('box');
 
-  const inputRaza =document.createElement('INPUT')
+  const inputRaza = document.createElement('INPUT');
   inputRaza.classList.add('raza');
 
-  const inputEdad =document.createElement('INPUT')
+  const inputEdad = document.createElement('INPUT');
   inputEdad.classList.add('edad');
 
-  const ingresar = document.createElement("img");//crea imagen foto
-  ingresar.src = "imagenes/pata1.png";
-  ingresar.classList.add("ingresar");
-  const link =document.createElement("a");
-  link.href="/wall";
+  const ingresar = document.createElement('img'); // crea imagen foto
+  ingresar.src = 'imagenes/pata1.png';
+  ingresar.classList.add('ingresar');
+  const link = document.createElement('a');
+  link.href = '/wall';
   link.append(ingresar);
 
-
-
-  body.append(img,inputFile,Nombre, inputNombre,Raza,inputRaza,Edad,inputEdad, link);
+  body.append(
+    img,
+    inputFile,
+    Nombre,
+    inputNombre,
+    Raza,
+    inputRaza,
+    Edad,
+    inputEdad,
+    link
+  );
   article.append(body);
   return article;
 };
